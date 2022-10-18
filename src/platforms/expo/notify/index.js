53fb4fc321404c08526d66127a5db5b5e0aff2e3
borispipo <borispipo@gmail.com>
2022-10-18 06:40:35 +0100

    Fix bug Uncaught (in promise) ReferenceError: type is not defined

--- src/platforms/expo/notify/index.js
+++ src/platforms/expo/notify/index.js
@@ -10,7 +10,7 @@ import { notifyRef,notificationRef,TYPES} from "$cnotify";
 export {default} from "$cnotify";
 
 notifyRef.current = (options)=>{
-    if(type == TYPES.info && isAndroid()){
+    if(options.type == TYPES.info && isAndroid()){
         return ToastAndroid.show(React.getTextContent(options.message),options.interval);
     }
     if(!notificationRef.current || !notificationRef.current.alertWithType) return;
