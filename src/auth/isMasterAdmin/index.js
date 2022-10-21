284067c0c51ae19900acdd45e9f0fe925975ca54
borispipo <borispipo@gmail.com>
2022-10-21 05:08:49 +0100

    Fix bug : Uncaught Error: Cannot find module './utils/session'

--- src/auth/isMasterAdmin/index.js
+++ src/auth/isMasterAdmin/index.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import { getLoggedUser } from "./utils/session";
+import { getLoggedUser } from "../utils/session";
 import isMasterAdmin from "$isMasterAdmin";
 import {defaultObj} from "$utils";
 
