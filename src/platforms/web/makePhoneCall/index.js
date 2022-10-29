5e436f88daac115c4f0bef6a00a9c732fd44fdc3
borispipo <borispipo@gmail.com>
2022-10-29 08:25:13 +0100

    Exporter les fonction makePhoneCall en environnement web

--- src/platforms/web/makePhoneCall/index.js
+++ src/platforms/web/makePhoneCall/index.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-export default x=>x;
-
 export const makePhoneCall = x=>Promise.reject({});
+
+export default makePhoneCall;
+
+export const canMakePhoneCall = x=>false;
