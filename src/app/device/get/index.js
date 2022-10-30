36787314a2167a69063e91ae78ed0c7a22d6eec7
borispipo <borispipo@gmail.com>
2022-10-30 09:10:24 +0100

    Fix bug @fto-consult/common/src/app/device/get/index.js:2 Module not found: Can't resolve './utils'

--- src/app/device/get/index.js
+++ src/app/device/get/index.js
@@ -1,5 +1,5 @@
 import session from "$session"
-import { sessionName } from "./utils";
+import { sessionName } from "../utils";
 import defaultStr from "$cutils/defaultStr";
 
 /***permet de récupérer la valeur du divice */
