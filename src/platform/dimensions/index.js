511aedb713504bd24ef2650c8fb88f33cec08351
borispipo <borispipo@gmail.com>
2022-11-25 09:12:32 +0100

    Fix bug Dimensions[v].bind is not function en environnement native expo

--- src/platform/dimensions/index.js
+++ src/platform/dimensions/index.js
@@ -8,7 +8,9 @@ import { useWindowDimensions } from "./utils";
 export * from "./utils";
 
 ['get','addEventListener','removeEventListener','set'].map((v)=>{
-    Dim[v] = Dimensions[v].bind(Dimensions);
+    if(typeof Dimensions[v] =='function'){
+        Dim[v] = Dimensions[v].bind(Dimensions)
+    }
 });
 
 export default Dim;
