f8fcedee38722496c48bb0a0b0afdf1ab9b3c7f1
borispipo <borispipo@gmail.com>
2022-11-28 08:16:43 +0100

    Fix bug i18n.js:7 Uncaught TypeError: Cannot read properties of undefined (reading 'dictionary')

--- src/utils/filters/i18n.js
+++ src/utils/filters/i18n.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import i18n from "./i18n";
+import i18n from "$i18n";
 
 i18n.dictionary({
     fr : {
