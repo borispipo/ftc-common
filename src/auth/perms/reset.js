3cdd1224e4baf043c8723c9f32c92d824cb9bd4d
borispipo <borispipo@gmail.com>
2022-12-04 08:24:11 +0100

    update package aushSignIn prise en compte des pros tables et tablesNames dans les perms de structdata

--- src/auth/perms/reset.js
+++ src/auth/perms/reset.js
@@ -4,7 +4,13 @@ export const structDataPerms = {};
 import tablesObj from"$pouchdbTableData";
 ///pointe vers l'ensemble des struct data de l'application
 import  structData from"$pouchdbStructData";
+import authSignIn2Signout from "../$authSignIn2SignOut";
 
+/*** 
+ *  - on peut définir la liste des noms de tables data dans le propriété tables|tableNames de auhSignIn2Signout
+ *  - on peut définir la liste des noms struct data dans la propriété structData | structDataTableNames de authSignIn2Signout
+ *  
+ */
 
 export const resetPerms = ()=>{
     const allPerms = {};
@@ -15,12 +21,14 @@ export const resetPerms = ()=>{
         delete structDataPerms[i];
     })
     const action = ['read','create','write','update','edit','delete','remove']
-    Object.map(tablesObj,(table,tableName,c1)=>{
+    Object.map(defaultObj(authSignIn2Signout.tables,authSignIn2Signout.tableNames,tablesObj),(table,tableName,c1)=>{
+        if(!isNonNullString(tableName)) return null;
         tableName = tableName.toLowerCase().trim();
         let resource = "table/"+tableName.ltrim("table/");
         tableDataPerms[tableName] = Auth.isAllowed({resource,action});
     })
-    Object.map(structData,(table,tableName,c1)=>{
+    Object.map(defaultObj(authSignIn2Signout.structData,authSignIn2Signout.structDataTableNames,structData),(table,tableName,c1)=>{
+        if(!isNonNullString(tableName)) return null;
         tableName = tableName.toLowerCase().trim();
         let resource = "structdata/"+tableName.ltrim("structdata/");
         structDataPerms[tableName] = Auth.isAllowed({resource,action});
