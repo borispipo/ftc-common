5a69455646749c4f0797e2c5b88985c26f27d3e3
boris-pashmotors <boris@pashmotors.com>
2023-01-12 22:29:27 +0100

    Affiche le message de log uniquement lorsqu'on envisage autre chose

--- src/app/config.js
+++ src/app/config.js
@@ -360,7 +360,7 @@ export const getTheme  =x=>{
 
 export const prefixStrWithAppId = (text,sep)=>{
     const appId = getAppId();
-    if(!isNonNullString(appId) && typeof window !='undefined' && typeof window =='object' && window){
+    if(isInitializedRef.current && !isNonNullString(appId) && typeof window !='undefined' && typeof window =='object' && window){
         console.error("Id de l'application non définie dans le fichier de configuration. vous devez le définir comme chaine de caractère non nulle dans la propriété appId ou id de ce même fichier");
     }
     if(typeof text !=="string") return appId;
