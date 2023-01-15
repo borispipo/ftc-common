24430ee4f9f5090539743254bd4780a83ac61411
boris-pashmotors <boris@pashmotors.com>
2023-01-15 11:02:15 +0100

    Installer à nouveau le composant stable-hash

--- src/utils/react/useStableMemo.js
+++ src/utils/react/useStableMemo.js
@@ -2,14 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import areEquals from '../compare';
-import { useMemo,useEffect, useRef } from "react";
+import stableHash from 'stable-hash';
+import { useMemo } from 'react';
 
 export default function useStableMemo(factory, deps){
-    const ref = useRef(null);
-    const areDifferents = !areEquals(ref.current,deps);
-    if(areDifferents){
-        ref.current = deps;
-    }
-    return useMemo(factory, [areDifferents]);
+    return useMemo(factory, [stableHash(deps)]);
 }
