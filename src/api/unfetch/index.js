11212c99a55809f8c9f4f5b33b51bd87e8e99086
boris-pashmotors <boris@pashmotors.com>
2023-01-21 18:30:37 +0100

    Update du package pour corriger le problème lié à l'alias unfetch en environnement node

--- src/api/unfetch/index.js
+++ src/api/unfetch/index.js
@@ -2,6 +2,8 @@ let unfetch = typeof window !=='undefined' && window && typeof XMLHttpRequest !=
 if(!unfetch){
     if(typeof global !='undefined' && global && typeof global.fetch =='function'){
         unfetch = global.fetch;
+    } else if(typeof globalThis != "undefined" && globalThis && typeof globalThis.fetch =='function'){
+        unfetch = globalThis.fetch;
     }
 }
 export default unfetch;
