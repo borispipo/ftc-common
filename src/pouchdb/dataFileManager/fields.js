990269c2f748b40387f1f02fa33399278867e096
boris-pashmotors <boris@pashmotors.com>
2023-03-16 17:56:49 +0100

    Update dataFileManager fields, make dataFile's code to lower case

--- src/pouchdb/dataFileManager/fields.js
+++ src/pouchdb/dataFileManager/fields.js
@@ -1,16 +1,24 @@
 /**** les différentes champs des fichiers de données */
 import types from "./types";
+import {sanitizeFileName} from "$cutils";
+
 export default {
     _id : {
         visibleOnlyOnEditing : true,
         type : "string",
         label : "Id",
+        lower : true,
+        primaryKey : true,
     },
     code : {
         label : "Code",
         type : "string",
         readOnlyOnEditing : true,
-        primaryKey : true,
+        lower : true,
+        required : true,
+        toCase : (value)=>{
+            return sanitizeFileName(value).toLowerCase();
+        }
     },
     type : {
         type : "select",
