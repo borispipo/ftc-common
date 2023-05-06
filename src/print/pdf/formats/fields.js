ce952e9205cbd616a576d838f1d4bc8d2a4d7a8b
boris-pashmotors <boris@pashmotors.com>
2023-05-06 12:37:17 +0100

    Prise en compte des permission

--- src/print/pdf/formats/fields.js
+++ src/print/pdf/formats/fields.js
@@ -17,8 +17,8 @@ export default {
         type : "selecttabledata",
         text : "Accessible aux utilisateurs",
         multiple : true,
-        idfield : 'code',
-        table : "users",
+        foreignKeyColumn : 'code',
+        foreignKeyTable : "users",
         dbName : "users"
     },
     displayLogo : {
