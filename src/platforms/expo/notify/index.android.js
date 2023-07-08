d6697bfe6a60e787bb8034a5ae7a2a6c946522b6
boris-pashmotors <boris@pashmotors.com>
2023-07-08 18:16:56 +0100

    Fix bug : Can't resolve 'react-native-web/dist/exports/ToastAndroid'

--- src/platforms/expo/notify/index.android.js
+++ src/platforms/expo/notify/index.android.js
@@ -1,12 +1,17 @@
 // Copyright 2022 @fto-consult/Boris Fouomene. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-import { notifyRef,notificationRef} from "$cnotify";
+import {ToastAndroid } from 'react-native';
+import React from "react";
+import {isAndroid} from "$cplatform";
+import { notifyRef,notificationRef,TYPES} from "$cnotify";
 
 export {default} from "$cnotify";
 
 notifyRef.current = (options)=>{
+    if(options.type == TYPES.info && isAndroid()){
+        return ToastAndroid.show(React.getTextContent(options.message),options.interval);
+    }
     if(!notificationRef.current || !notificationRef.current.alertWithType) return;
     return notificationRef.current.alertWithType(options);
 }
