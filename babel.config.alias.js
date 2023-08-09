924a84cd6ea207df6ff39868500329933e0620bf
boris-pashmotors <boris@pashmotors.com>
2023-08-09 08:04:54 +0100

    Supprimer l'alias base en lieu et place de projectRoot

--- babel.config.alias.js
+++ babel.config.alias.js
@@ -14,7 +14,7 @@ const getPlatform = (platform)=>{
 /*** 
  * @param {object}, les options du resolver, objet de la forme : 
  * {
- *      base : {string}, le chemin racine du projet
+ *      projectRoot : {string}, le chemin racine du projet
  *      platform : {web||expo},//La plateforme du client
  *      assets : {string}, le chemin assets de l'application,
  *      alias  : {object}, les alias supplémentaires, pouvant être concaténés à ceux généré
@@ -25,13 +25,13 @@ module.exports = function(opts){
     /***** faire une copie du fichier package.json, situé à la racine du projet */
     const rootDir = path.resolve(__dirname);
     const common = path.resolve(rootDir,"src");
-    let {base,withPouchdb,withPouchDB,assets,alias,platform} = opts && typeof opts =="object"? opts : {};
+    let {projectRoot,withPouchdb,withPouchDB,assets,alias,platform} = opts && typeof opts =="object"? opts : {};
     platform = getPlatform(platform);
     withPouchDB = withPouchDB || withPouchdb;
-    base = base && typeof base =='string' && fs.existsSync(base) ? base : process.cwd();;
-    const src = path.resolve(base,"src");
-    const packagePath = path.resolve(base,"package.json");
-    const configPath = path.resolve(base,"app.config.json");
+    projectRoot = projectRoot && typeof projectRoot =='string' && fs.existsSync(projectRoot) ? projectRoot : process.cwd();;
+    const src = path.resolve(projectRoot,"src");
+    const packagePath = path.resolve(projectRoot,"package.json");
+    const configPath = path.resolve(projectRoot,"app.config.json");
     if(fs.existsSync(packagePath)){
         try {
             const packageObj = require(`${packagePath}`);
@@ -79,8 +79,8 @@ module.exports = function(opts){
         "$curi" : path.resolve(common,"utils","uri"),
         "$ccurrency" : path.resolve(common,"lib","currency"),
         "$csession" : path.resolve(common,"session"),
-        "$cbase64" : path.resolve(common,"lib","base-64"),
-        "$base64" : path.resolve(common,"lib","base-64"),
+        "$cbase64" : path.resolve(common,"lib","projectRoot-64"),
+        "$base64" : path.resolve(common,"lib","projectRoot-64"),
         "$cactions" : path.resolve(common,"actions"),
         
         "$media" : path.resolve(common,"media"),
@@ -104,7 +104,8 @@ module.exports = function(opts){
         "$currency" : path.resolve(common,"lib","currency"),
         "$session" : path.resolve(common,"session"),
         "$actions" : path.resolve(common,"actions"),
-        "$base" :base, 
+        "$projectRoot" :projectRoot, 
+        "$base" : $projectRoot,
         "$src" : src,
         "$cdatafileManager": cdataFileManager,
         "$datafileManager": cdataFileManager,
