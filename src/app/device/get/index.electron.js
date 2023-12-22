432973caf3481a2abf3e6293ca0dfd4267d43303
boris-pashmotors <boris@pashmotors.com>
2023-12-22 11:12:15 +0100

    Fix bug sur device.get en environnement electron

--- src/app/device/get/index.electron.js
+++ src/app/device/get/index.electron.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 import get from "./index.js";
-
+import {isElectron} from "$cplatform";
 export default function getElectronDeviceId (){
-    return get() || ELECTRON?.DEVICE?.computerName;
+    return get() || isElectron() && ELECTRON?.DEVICE?.computerName || '';
 }
