5c09ceba37385891451a85cdf02879c35bda9c0f
boris-pashmotors <boris@pashmotors.com>
2024-02-01 22:05:28 +0100

    Supprimer le pacakge react-native-get-random-values

--- src/pouchdb/pouchdb/index.native.js
+++ src/pouchdb/pouchdb/index.native.js
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 
 import mobileNativePouchAdapter from "./native.adapter";
-import 'react-native-get-random-values';
 import  PouchDB from "pouchdb";
 
 const extra = {adapter : mobileNativePouchAdapter.adapter};
